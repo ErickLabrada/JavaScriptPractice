efc264611062f9ca143c44e2ad4c4228a372ee9f
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2024-03-29 17:08:20 -0700

    <Set-up> Did the set up and went over the "basics" all over again, turns out js is pretty much python

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function sum(n1, n2){
+    return n1+n2;
+}
+
+console.log(sum(2,3));
+
+
+function substract(n1,n2){
+    return n1-n2;
+}
+
+console.log(substract(5,2));
+console.log(substract(2,5));
